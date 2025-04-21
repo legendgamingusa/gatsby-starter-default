61798f9e9ad13a80c10f917a7d58f3964fde4f84
legendgamingusa <legendgamingfb@gmail.com>
2025-04-21 18:16:20 -0500

    page-2 update

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,7 +7,7 @@ import Seo from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <p>Welcome to page 2, my name is Legend and I am learning how to build on this platform.</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
